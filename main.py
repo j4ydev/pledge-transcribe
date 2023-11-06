be002edbb8673d10a6c89f8f069510a551e46e2a
j4ydev <zankat.jaydevsinh.r@gmail.com>
2023-11-07 00:07:21 +0530

    create class transcribe. save data in csv file.

--- main.py
+++ main.py
@@ -8,63 +8,59 @@ from icecream import ic
 model = whisper.load_model("large")
 import demucs.separate
 
-
 import pandas as pd
 
-df = pd.DataFrame(columns=['videoFilePath', 'name', 'transcribe', 'pledge_score'])
+class TRANSCRIBE():
+    def __init__(self):
+        self.df = pd.DataFrame(columns=['videoFilePath','transcribe_text'])
 
-listOfDirs = glob.glob("folder*")
-listOfDirs.sort()
+    def transcribe(self, agentName):
+        demucs.separate.main(["--mp3", "--two-stems", "vocals", "-n", "mdx_extra", agentName])
+        audioPath = f"separated/mdx_extra/{agentName.split('/')[-1].replace('.mp4', '')}/vocals.mp3"
 
-def transcribe(agentName):
-    demucs.separate.main(["--mp3", "--two-stems", "vocals", "-n", "mdx_extra", agentName])
-    audioPath = f"separated/mdx_extra/{agentName.split('/')[-1].replace('.mp4', '')}/vocals.mp3"
+        options = dict(language="en", beam_size=5, best_of=5)
+        transcribeOption = dict(task="transcribe", **options)
+        transcription = model.transcribe(audioPath, **transcribeOption)
 
-    options = dict(language="en", beam_size=5, best_of=5)
-    transcribeOption = dict(task="transcribe", **options)
-    transcription = model.transcribe(audioPath, **transcribeOption)
+        transcribedText = transcription["text"]
 
-    transcribedText = transcription["text"]
-    return transcribedText
+        return transcribedText
 
-def find_name(transcribedText):
-    NER = spacy.load("en_core_web_sm")
-    text1= NER(transcribedText)
+    def process(self, dir_path, csv_path):
+        dir = dir_path 
+        filesList = glob.glob(f"{dir}/*.mp4")
+        filesList.sort()
+        print(filesList)
 
-    name = "not found"
-    for word in text1.ents:
-        # print(word.text,word.label_)
-        if word.label_ == "PERSON":
-          name = word.text
-    return name
+        for files in filesList:
+            print(files)
+            # transcribe text
+            start_time = time.time()
+            transcribedText = self.transcribe(files)
+            end_time = time.time()
+            calculate_time = end_time - start_time
+
+            print("##" * 20)
+            ic(transcribedText)
+            ic(calculate_time)
+            print("##" * 20)
+
+            #update database
+            new_row = {
+            'videoFilePath': files.split("/")[-1].replace(".mp4",""),
+            'transcribe_text': transcribedText,
+            }
+
+            new_df = pd.DataFrame(new_row, index=[0])
+            self.df = pd.concat([self.df, new_df], ignore_index=True)
+
+        # Save the DataFrame as a CSV file
+        self.df.to_csv(csv_path, index=False)
 
 
-def calculate_sililarity_score(transcribedText):
-    actualString = f"pledge to educate myself and others about the potential effects of antimicrobial resistance and help protect tomorrow. Thank you."
-    similarityScore = fuzz.partial_ratio(actualString, transcribedText)
-    return similarityScore
-
-print("--" * 20)
-# for dir in listOfDirs:
-dir = "folder_2" #path of the directory
-filesList = glob.glob(f"{dir}/*.mp4")
-filesList.sort()
-
-print(filesList)
-
-for files in filesList:
-    print(files)
-    start_time = time.time()
-    transcribedText = transcribe(files)
-    name = find_name(transcribedText)
-    #similarityScore = calculate_sililarity_score(transcribedText)
-    
-    end_time = time.time()
-
-    calculate_time = end_time - start_time
-    
-    print("##" * 20)
-    ic(transcribedText)
-    ic(calculate_time)
-    print("##" * 20)
-
+####
+if __name__ == "__main__":
+    dir = "/Users/jay/work/transcribe_pledge/all_mp4" ### DIRECTORY OF VIDEO FILES (DO NOT ADD / AT THE END OF THE PATH)
+    csv_path = "transcribe_text.csv" ### PATH OF THE CSV FILE
+    transcribe_obj = TRANSCRIBE()
+    transcribe_obj.process(dir, csv_path)
--- readme.txt
+++ readme.txt
@@ -1,4 +1,6 @@
 Install python=3.9.6
+Install ffmpeg - brew install ffmpeg
+
 --------------------------------------------
 
 install requirements file.
@@ -7,3 +9,10 @@ pip install -r requirements.txt
 
 Move video direcories in this directory.
 in line no 49 of main.py change the name of the folder.
+
+
+
+###  Instructions  ###
+
+if # comments - It is instructions for code understanding.
+if ### comment - Change according to your preference.
