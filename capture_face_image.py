7cd35e58e3f0b951d4b062b0cba8d34bd61fa84e
codexsid <sidd@hafla.com>
2023-11-20 02:18:50 +0530

    align config var usage

--- capture_face_image.py
+++ capture_face_image.py
@@ -7,7 +7,7 @@ from deepface import DeepFace
 from icecream import ic
 from numpy.linalg import norm
 
-from config import *  # TODO: Import only needed names or import the module and then use its members. google to know more
+from config import *  # # TODO: Import only needed names or import the module and then use its members. google to know more
 
 
 class GETFRAME():
@@ -31,8 +31,8 @@ class GETFRAME():
         if current_frame == random_frame:
             return frame
 
-    def check_if_screenshot_present(self, image_file_name):
-        possible_screenshot_path = f"{FACE_IMAGE_DIRECTORY}/{image_file_name}"
+    def check_if_screenshot_present(self, image_file_name_without_extension):
+        possible_screenshot_path = f"{FACE_IMAGE_DIRECTORY}/{image_file_name_without_extension}{FACE_IMAGE_FILE_FORMAT}"
         if os.path.isfile(possible_screenshot_path):
             return True
         else:
@@ -48,19 +48,19 @@ class GETFRAME():
         return found_face_flag
 
     def capture_face_image(self, video_file_path, image_file_path_without_extension):
-        reference_frame_file_path = f'{FACE_IMAGE_DIRECTORY}/{image_file_path_without_extension}.{FACE_IMAGE_FILE_FORMAT}'
-        frame_to_compare_file_path = f'{FACE_IMAGE_DIRECTORY}/{image_file_path_without_extension}_2.{FACE_IMAGE_FILE_FORMAT}'
+        reference_frame_file_path = f'{FACE_IMAGE_DIRECTORY}/{image_file_path_without_extension}{FACE_IMAGE_FILE_FORMAT}'
+        frame_to_compare_file_path = f'{FACE_IMAGE_DIRECTORY}/{image_file_path_without_extension}_2{FACE_IMAGE_FILE_FORMAT}'
 
         if self.counter == 0:
             print("Attempting to find face in video: ", image_file_path_without_extension)
 
         self.counter = self.counter + 1
 
-        frame_to_compare = self.get_image_from_video(video_file_path)
-        cv2.imwrite(frame_to_compare_file_path, frame_to_compare)
-
         reference_frame = self.get_image_from_video(video_file_path)
         cv2.imwrite(reference_frame_file_path, reference_frame)
+
+        frame_to_compare = self.get_image_from_video(video_file_path)
+        cv2.imwrite(frame_to_compare_file_path, frame_to_compare)
 
         if self.counter > 30:
             self.remove_file_if_exists(reference_frame_file_path)
@@ -75,17 +75,15 @@ class GETFRAME():
             print("Face Found, Attempts: ", self.counter)
             return
         else:
-            self.capture_face_image(video_file_path, reference_frame_file_path)
+            self.capture_face_image(video_file_path, image_file_path_without_extension)
 
     def process(self, input_video_files_list):
         for video_file_path in input_video_files_list:
-            # TODO: split filePath and fileName, then extract fileName without extension and then pass filePath, fileNameWithoutExtension
-            image_file_name = video_file_path.split("/")[-1].replace(".mp4", ".png") # Incorrect variable name image_file_name is not file name it is file path!!
-            image_file_path_without_extension = image_file_name.replace(".mp4", "")
-            screenshot_present_flag = self.check_if_screenshot_present(image_file_name)
+            image_file_name_without_extension = video_file_path.split("/")[-1].replace(INPUT_VIDEO_FILE_FORMAT, "")
+            screenshot_present_flag = self.check_if_screenshot_present(image_file_name_without_extension)
             if not screenshot_present_flag:
                 self.counter = 0
-                self.capture_face_image(video_file_path, image_file_path_without_extension)
+                self.capture_face_image(video_file_path, image_file_name_without_extension)
         return "Complete"
 
 if __name__ == "__main__":
