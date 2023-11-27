a8a3609217febdc0cc62714e18c47c155affcf85
codexsid <sidd@hafla.com>
2023-11-28 01:11:30 +0400

    add flag for both filename convention

--- utils.py
+++ utils.py
@@ -3,13 +3,19 @@ import numpy as np
 # TODO: Import only needed names or import the module and then use its members. google to know more
 from config import *
 
+USE_OLD_VIDEO_FILE_NAME = False
 
 # current video file name format is 500_6340863312112_naresh_ch.mp4
 def get_metadata_from_file_name(file_name):
     file_name_separate_list = file_name.split('_')
-    file_bid = "_"
-    file_video_id = file_name_separate_list[0]
-    file_name_suffix = file_name_separate_list[1]
+    if USE_OLD_VIDEO_FILE_NAME:
+        file_bid = "_"
+        file_video_id = file_name_separate_list[0]
+        file_name_suffix = file_name_separate_list[1]
+    else:
+        file_bid = file_name_separate_list[0]
+        file_video_id = file_name_separate_list[1]
+        file_name_suffix = file_name_separate_list[2]
     return file_bid, file_video_id, file_name_suffix
 
 def is_value_present_in_dataframe(file_video_id, dataframe):
