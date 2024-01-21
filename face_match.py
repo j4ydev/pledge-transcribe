e40e3b6f25719e1a7140a9b0f2caca27228d7747
j4ydev <zankat.jaydevsinh.r@gmail.com>
2024-01-21 15:17:23 +0530

    save 5 most similar images in a directory.

--- face_match.py
+++ face_match.py
@@ -32,57 +32,66 @@ class MATCHFACE():
     def process(self, face_images_list):
 
         for face_image_path in face_images_list:
-            ic(face_image_path)
-            match_face_image_file_name = face_image_path.split("/")[-1].replace(".png", "")
-            file_bid, video_id, file_name_suffix = get_metadata_from_file_name(match_face_image_file_name)
-            is_value_present_flag = is_value_present_in_dataframe(video_id, self.face_match_dataframe)
+            
+            if face_image_path[-4:] == ".png":
+                match_face_image_file_name = face_image_path.split("/")[-1].replace(".png", "")
+                file_bid, video_id, file_name_suffix = get_metadata_from_file_name(match_face_image_file_name)
+                is_value_present_flag = is_value_present_in_dataframe(video_id, self.face_match_dataframe)
 
-            if not is_value_present_flag:
+                if not is_value_present_flag:
+                    print("--" * 200)
+                    dfs = DeepFace.find(img_path = face_image_path, db_path = FACE_IMAGE_DIRECTORY, distance_metric="cosine", model_name="Facenet512", enforce_detection=False)
+                    print("NUMBER_OF_BEST_MATCH_TO_CONSIDER: ", self.NUMBER_OF_BEST_MATCH_TO_CONSIDER)
+                    print(type(self.NUMBER_OF_BEST_MATCH_TO_CONSIDER))
+                    match_face_paths = list(dfs[0].head(self.NUMBER_OF_BEST_MATCH_TO_CONSIDER)["identity"])
+                    match_face_distance = list(dfs[0].head(self.NUMBER_OF_BEST_MATCH_TO_CONSIDER)["Facenet512_cosine"])
+                    ic(match_face_distance)
 
-                dfs = DeepFace.find(img_path = face_image_path, db_path = FACE_IMAGE_DIRECTORY, distance_metric="cosine", model_name="Facenet512")
-                print("NUMBER_OF_BEST_MATCH_TO_CONSIDER: ", self.NUMBER_OF_BEST_MATCH_TO_CONSIDER)
-                print(type(self.NUMBER_OF_BEST_MATCH_TO_CONSIDER))
-                match_face_paths = list(dfs[0].head(self.NUMBER_OF_BEST_MATCH_TO_CONSIDER)["identity"])
-                match_face_distance = list(dfs[0].head(self.NUMBER_OF_BEST_MATCH_TO_CONSIDER)["Facenet512_cosine"])
-                ic(match_face_distance)
+                    temp_NUMBER_OF_BEST_MATCH_TO_CONSIDER = self.NUMBER_OF_BEST_MATCH_TO_CONSIDER
+                    if len(match_face_paths) < self.NUMBER_OF_BEST_MATCH_TO_CONSIDER:
+                        temp_NUMBER_OF_BEST_MATCH_TO_CONSIDER = len(match_face_distance)
+                    match_found = "False"
+                    new_face_match_row = {'video_id': video_id, 'match_found': match_found}
 
-                temp_NUMBER_OF_BEST_MATCH_TO_CONSIDER = self.NUMBER_OF_BEST_MATCH_TO_CONSIDER
-                if len(match_face_paths) < self.NUMBER_OF_BEST_MATCH_TO_CONSIDER:
-                    temp_NUMBER_OF_BEST_MATCH_TO_CONSIDER = len(match_face_distance)
-                match_found = "False"
-                new_face_match_row = {'video_id': video_id, 'match_found': match_found}
+                    for j in range(0,temp_NUMBER_OF_BEST_MATCH_TO_CONSIDER):
+                        if len(match_face_distance) >= 2:
+                            if match_face_distance[1] < 0.80:
+                                match_found = "True"
+                                new_face_match_row[f"match_found"] = match_found
+                            
+                        _, face_video_id, _ = get_metadata_from_file_name(match_face_paths[j].split("/")[-1])
+                        new_face_match_row[f"face_match_{j+1}"] = face_video_id
+                        new_face_match_row[f"distance_indicator_{j+1}"] = match_face_distance[j]
+                        # new_face_match_row = {'video_id': video_id, 'match_found': match_found, 'face_match_1': face_match_person_1_video_id, 'distance_indicator_1': match_face_distance[0], 'face_match_2': face_match_person_2_video_id, 'distance_indicator_2': match_face_distance[1]}
 
-                for j in range(0,temp_NUMBER_OF_BEST_MATCH_TO_CONSIDER):
-                    if len(match_face_distance) >= 2:
-                        if match_face_distance[1] < 0.30:
-                            match_found = "True"
-                            new_face_match_row[f"match_found"] = match_found
+                    print("##" * 20)
+                    ic(new_face_match_row)
+                    print("##" * 20)
+
+                    new_face_match_dataframe = pd.DataFrame(new_face_match_row, index=[0])
+                    self.face_match_dataframe = pd.concat([self.face_match_dataframe, new_face_match_dataframe], ignore_index=True)
+                    self.face_match_dataframe.to_csv(FACE_MATCH_RESULT_CSV_PATH, index=False)
+
+                    ic(video_id)
+                    for i in range (0, len(match_face_paths)):
+                        individual_face_match_directory = f"{FACE_MATCH_SAVE_IMAGES_DIRECTORY}/{video_id}"
+                        if not os.path.isdir(individual_face_match_directory):
+                            os.mkdir(individual_face_match_directory)
+
+                        match_file_bid, match_video_id, match_file_name_suffix = get_metadata_from_file_name(match_face_paths[i].split("/")[-1])
+
+                        print("**" * 20)
+                        ic(match_file_name_suffix)
+                        ic(match_video_id)
+                        print("**" * 20)
+
+                        similar_face_image_path = f"{individual_face_match_directory}/{i+1}_{'{:04d}'.format(int(match_face_distance[i]*1000))}_{match_video_id}_{match_file_name_suffix}.png"
+                        ic(individual_face_match_directory)
                         
-                    _, face_video_id, _ = get_metadata_from_file_name(match_face_paths[j].split("/")[-1])
-                    new_face_match_row[f"face_match_{j+1}"] = face_video_id
-                    new_face_match_row[f"distance_indicator_{j+1}"] = match_face_distance[j]
-                    # new_face_match_row = {'video_id': video_id, 'match_found': match_found, 'face_match_1': face_match_person_1_video_id, 'distance_indicator_1': match_face_distance[0], 'face_match_2': face_match_person_2_video_id, 'distance_indicator_2': match_face_distance[1]}
-
-                print("##" * 20)
-                ic(new_face_match_row)
-                print("##" * 20)
-
-                new_face_match_dataframe = pd.DataFrame(new_face_match_row, index=[0])
-                self.face_match_dataframe = pd.concat([self.face_match_dataframe, new_face_match_dataframe], ignore_index=True)
-                self.face_match_dataframe.to_csv(FACE_MATCH_RESULT_CSV_PATH, index=False)
-
-                ic(video_id)
-                for i in range (0, len(match_face_paths)):
-                    individual_face_match_directory = f"{FACE_MATCH_SAVE_IMAGES_DIRECTORY}/{video_id}"
-                    if not os.path.isdir(individual_face_match_directory):
-                        os.mkdir(individual_face_match_directory)
-
-                    match_file_bid, match_video_id, match_file_name_suffix = get_metadata_from_file_name(match_face_paths[i])
-                    similar_face_image_path = f"{individual_face_match_directory}/{i+1}_{'{:04d}'.format(int(match_face_distance[i]*1000))}_{match_video_id}_{match_file_name_suffix}.png"
-                    ic(similar_face_image_path)
-                    ic(match_face_paths[i])
-                    print("--" * 40)
-                    shutil.copy(match_face_paths[i], similar_face_image_path)
+                        ic(similar_face_image_path)
+                        ic(match_face_paths[i])
+                        print("--" * 40)
+                        shutil.copy(match_face_paths[i], similar_face_image_path)
 
 
 
