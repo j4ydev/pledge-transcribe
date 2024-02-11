a0b7e857aa57f7ae8e14a41c328b1c9d72d9727f
codexsid <sidd@hafla.com>
2024-02-11 11:21:33 +0400

    relocate file

--- scraping_utils/rename_screenshots.py
+++ scraping_utils/rename_screenshots.py
@@ -1,7 +1,6 @@
 import glob
 import os
 
-
 ### Handled cases:
 # 1. when there is vid_fname_lname.png file. We will not rename the file.
 # 2. when there is vid_frame_lname_.png file. We will not rename the file.
@@ -10,20 +9,20 @@ import os
 
 # TO ADD 1 LAYER OF ASSURANCE:: before removing any id it will check if id term is int or not if not then we will not remove the id term.
 
-
-screenshot_image_list = glob.glob("screenshots/*.png") ## update the location of screenshot directory.
+#
+screenshot_image_list = glob.glob("/Users/khasgiwa/Workbench/codebench/a7v/pledge-transcribe/output/screenshots/*.png") ## update the location of screenshot directory.
 print(screenshot_image_list[0])
 
 for image_path in screenshot_image_list:
     image_name = image_path.split("/")[-1].replace(".png", "")
 
     image_name_naming_convention_elements_list = image_name.split("_")
-    
+
     if "" in image_name_naming_convention_elements_list:
         image_name_naming_convention_elements_list.remove("")
 
     print(image_name_naming_convention_elements_list)
-    
+
     if len(image_name_naming_convention_elements_list) >= 4:
         print(f"{image_name_naming_convention_elements_list[0]}_")
 
