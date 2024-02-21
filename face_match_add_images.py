3f07bfc3247d28d7abacc302c197cd65ad30a157
codexsid <sidd@hafla.com>
2024-02-21 23:18:39 +0400

    auto format

--- face_match_add_images.py
+++ face_match_add_images.py
@@ -1,10 +1,15 @@
-import requests
 import glob
 import os
-from config import ADD_IMAGES_TO_API_CSV, ADD_IMAGES_TO_API_ERROR_CSV, FINAL_FACES_DIRECTORY, FACE_IMAGE_FILE_FORMAT, API_KEY
-import pandas as pd
-from utils import get_metadata_from_file_name, is_value_present_in_dataframe
 import time
+
+import pandas as pd
+import requests
+
+from config import (ADD_IMAGES_TO_API_CSV, ADD_IMAGES_TO_API_ERROR_CSV,
+                    API_KEY, FACE_IMAGE_FILE_FORMAT, FINAL_FACES_DIRECTORY)
+from utils import get_metadata_from_file_name, is_value_present_in_dataframe
+
+
 class ADDFACES():
     def __init__(self):
 
@@ -42,6 +47,7 @@ class ADDFACES():
         if not is_value_present_flag:
             try:
                 files = {"file": open(face_path, "rb")}
+                print(f"{video_id_}: uploading  {face_path}")
                 response = requests.post(
                     self.add_face_url, data=self.payload, files=files, headers=self.headers
                 )
@@ -51,7 +57,7 @@ class ADDFACES():
                 face_id = eval(response.text)["amazon"]["face_ids"][0]
 
                 # print("---------------------------------------------------------------------------------")
-                
+
                 new_add_images_to_api_raw = {'video_id': str(video_id_), 'image_file_path' : str(face_path), 'face_id': str(face_id)}
                 new_add_images_to_api_dataframe = pd.DataFrame(new_add_images_to_api_raw, index=[0])
 
@@ -67,7 +73,7 @@ class ADDFACES():
 
 if __name__ == "__main__":
     add_faces_obj = ADDFACES()
-    
+
     face_images_list = glob.glob(f"{FINAL_FACES_DIRECTORY}/*{FACE_IMAGE_FILE_FORMAT}")
     print(face_images_list)
     for face_image_path in face_images_list:
